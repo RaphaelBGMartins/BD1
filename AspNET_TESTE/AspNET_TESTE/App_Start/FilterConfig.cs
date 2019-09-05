76212da8ccd289f0f39c758c8ec3009cd90fb1ff
Raphael B. G. Martins <raphaelbgmartins@hotmail.com>
2019-09-04 21:26:49 -0300

    Primeiro commit

---
+++ AspNET_TESTE/AspNET_TESTE/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNET_TESTE {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
